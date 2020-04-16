95879540e2e958caf3516aa1d1ff91bce925ca1c
yeongunheo <gjduddns@gmail.com>
2020-04-16 15:37:02 +0900

    feat: 요구사항1. index.html 페이지요청에 대한 응답처리 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,15 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.Reader;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,10 +29,26 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+        	Reader reader = new InputStreamReader(in);
+        	BufferedReader br = new BufferedReader(reader);
+        	
+        	String line = br.readLine();
+        	if (line == null) { return; }
+        	log.debug("request: {}", line);
+
+        	String[] tokens = line.split(" ");
+        	String url = tokens[1];
+        	while (!"".equals(line)) {
+        		line = br.readLine();
+        		log.debug("header: {}", line);
+        	}
+
+        	DataOutputStream dos = new DataOutputStream(out);
+        	File file = new File("./webapp" + url);
+        	byte[] body = Files.readAllBytes(file.toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
